e46a82ab2aaa8b751616e09b6fbb0c3817e1f41e
Eugene Stepanenko <jst1749.7@gmail.com>
2022-10-30 00:17:31 +0500

    add frontend

---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="room" v-for="room in rooms" v-bind:key="room.id">
+      <div><strong>Number: </strong>{{room.number}}</div>
+      <div><strong>Name: </strong>{{room.name}}</div>
+      <div><strong>Temperature: </strong>{{room.temperature}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      rooms: []
+    }
+  },
+  mounted() {
+    fetch("/api/v1/rooms/")
+        .then((response) => response.json())
+        .then((data) => {
+          this.rooms = data;
+        });
+  }
+}
+</script>
