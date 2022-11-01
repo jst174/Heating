201cc10ef2a98a341046360f1f10f393ba22501a
Eugene Stepanenko <jst1749.7@gmail.com>
2022-11-02 01:25:06 +0500

    add test for RoomService and add Builder for Room

--- src/main/java/com/stepanenko/heating/model/Room.java
+++ src/main/java/com/stepanenko/heating/model/Room.java
@@ -2,8 +2,10 @@ package com.stepanenko.heating.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,4 +22,50 @@ public class Room {
     @ManyToOne
     @JoinColumn(name = "floor_id", nullable = false)
     private Floor floor;
+
+    public static class Builder {
+
+        private Room room;
+
+        public Builder() {
+            room = new Room();
+        }
+
+        public Builder setId(Long id) {
+            room.setId(id);
+            return this;
+        }
+
+        public Builder setNumber(String number) {
+            room.setNumber(number);
+            return this;
+        }
+
+        public Builder setName(String name) {
+            room.setName(name);
+            return this;
+        }
+
+        public Builder setTemperature(int temperature) {
+            room.setTemperature(temperature);
+            return this;
+        }
+
+        public Room build(){
+            return room;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Room room = (Room) o;
+        return id != null && Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
